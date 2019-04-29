64bdbab656a6eb80beb7ef7a39f8707062a84db9
WANG Zhiwei <zwangcj@WIN2K.CS.UST.HK>
2019-04-29 16:59:47 +0800

    particle 1.0

---
+++ force.cpp
@@ -0,0 +1,23 @@
+#include "Force.h"
+
+#include "particle.h"
+
+
+
+void Gravity::addForce(Particle* particle)
+
+{
+
+	particle->setNetForce(particle->getNetForce() + g * particle->getMass());
+
+}
+
+
+
+void Viscous::addForce(Particle* particle)
+
+{
+
+	particle->setNetForce(particle->getNetForce() - K * particle->getSpeed());
+
+}
